81733cfa7eeb2b42b584f07ef9d451d3ef6c940b
ysshin93 <troy.yoonsik.shin@gmail.com>
2020-12-07 02:02:15 -0500

    home

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Home />
     </div>
--- Home.js
+++ Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './Home.css'
+import Profile from "./img/profile.jpg"
+import Cats from "./img/thor_chu.jpg"
 
 function Home() {
     return (
         <div className="home">
-            <h1>Hello World!</h1>
+            <div dlss="img__wrap">
+                <a href="https://www.instagram.com/yoonsik.e/"><img src={ Profile } ></img></a>
+                <p class="img__wrt"> Click Me! </p>
+            </div>
+            <a href="https://www.instagram.com/ddol.chu_/"> <img src={ Cats }></img> About my cats </a>
         </div>
     )
 }
